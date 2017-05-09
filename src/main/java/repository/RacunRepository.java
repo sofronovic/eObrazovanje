48557e78c91cd5091b3c529792b4a39005581ca5
Dusan Bulatovic <wubong99@gmail.com>
2017-05-09 13:58:35 +0200

    Controllers, DTO's, Repositorys, Services

---
+++ src/main/java/repository/RacunRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import model.Racun;
+
+public interface RacunRepository extends JpaRepository<Racun, Long>{
+
+}
